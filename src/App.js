06bd4e0caeada2e918d9bd4121e2a7d37784d6ef
rabin245 <drarabin@gmail.com>
2022-08-14 23:33:19 +0545

    start page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import blob_1 from "./images/blobs_1.png";
+import blob_2 from "./images/blobs_2.png";
+import Start from "./components/Start";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="blob-bottom" src={blob_1} alt="blob" />
+      <img className="blob-top" src={blob_2} alt="blob" />
+      <Start />
     </div>
   );
 }
---
+++ src/components/Start.js
@@ -0,0 +1,13 @@
+function Start(props) {
+  return (
+    <div className="start-page">
+      <h1 className="title">Quizzical</h1>
+      <p className="description">Quiz but with a 'zical' smiley face :-P</p>
+      <button className="btn">
+        <span>Start quiz</span>
+      </button>
+    </div>
+  );
+}
+
+export default Start;
