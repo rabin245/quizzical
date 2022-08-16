b175a496bf577c47cc9fbc5ccf2634151e9c0c59
rabin245 <drarabin@gmail.com>
2022-08-16 16:57:56 +0545

    choose answers and color them

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,30 +1,83 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 
 function Quiz(props) {
   const { data, handleAnswer } = props;
 
-  const questionList = data.map((data) => data.question);
+  const [questionList, setQuestionList] = useState(
+    data.map((data) => data.question)
+  );
+  const [answerList, setAnswerList] = useState(
+    data.map((data) => {
+      let answers = [...data.incorrect_answers, data.correct_answer];
+      answers = answers.sort(() => Math.random() - 0.5);
 
-  const answerList = data.map((data) => {
-    let answers = [...data.incorrect_answers, data.correct_answer];
-    answers = answers.sort(() => Math.random() - 0.5);
-    return answers;
-  });
+      return answers.map((answer) => ({
+        id: nanoid(),
+        answer: answer,
+        chosen: false,
+      }));
+    })
+  );
+
+  const answerButtonStyle = {
+    backgroundColor: "#d6dbf5",
+    border: "none",
+  };
+
+  function replaceEntities(str) {
+    const entities = {
+      "&#039;": "'",
+      "&quot;": '"',
+      "&amp;": "&",
+      "&lt;": "<",
+      "&gt;": ">",
+    };
+
+    return str.replace(
+      /&#039;|&quot;|&amp;|&lt;|&gt;/g,
+      (match) => entities[match]
+    );
+  }
+
+  function choseAnswer(index, id) {
+    setAnswerList((prevAnswers) => {
+      return prevAnswers.map((answersList, i) => {
+        if (i === index) {
+          return answersList.map((answer) => {
+            if (answer.id === id) {
+              return { ...answer, chosen: !answer.chosen };
+            }
+            return { ...answer, chosen: false };
+          });
+        } else {
+          return answersList;
+        }
+      });
+    });
+  }
 
   function returnQuestionBlock() {
     return questionList.map((question, index) => {
+      question = replaceEntities(question);
       return (
         <div className="question-block" key={index}>
           <h3 className="question">{question}</h3>
           <div className="answer-block">
-            {answerList[index].map((answer, index) => {
+            {answerList[index].map((answerObj, i) => {
+              const answer = replaceEntities(answerObj.answer);
               return (
                 <button
-                  className="answer btn"
-                  key={index}
-                  onClick={() => handleAnswer(answer, index)}
+                  className="answer"
+                  key={i}
+                  onClick={() => {
+                    console.log("clicked on", answer, i);
+                    choseAnswer(index, answerObj.id);
+                    handleAnswer(answer, index);
+                  }}
+                  style={answerObj.chosen ? answerButtonStyle : null}
                 >
-                  {answer}
+                  <span>{answer}</span>
                 </button>
               );
             })}
@@ -38,9 +91,12 @@ function Quiz(props) {
     <div className="quiz-page">
       <div className="page-block">{returnQuestionBlock()}</div>
 
-      <div>
-        checking answer
-        <button>Check Answers</button>
+      <div className="button-block">
+        {/* <span className="remarks">You scored 3/5 correct answers</span> */}
+        <button className="btn check">
+          <span>Check Answers</span>
+          {/* <span>Play again</span> */}
+        </button>
       </div>
     </div>
   );
